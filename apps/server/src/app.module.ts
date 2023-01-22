db95cf19f211836ec004601d7e4818f794fc6634
Idhibhat Pankam <marginpankam@gmail.com>
2023-01-22 23:58:14 +0700

    feat: setup prisma

--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -1,6 +1,10 @@
 import { Module } from "@nestjs/common";
 
+import { PrismaModule } from "./prisma/prisma.module";
+
+import { UserModule } from "./user/user.module";
+
 @Module({
-  imports: [],
+  imports: [UserModule, PrismaModule],
 })
 export class AppModule {}
