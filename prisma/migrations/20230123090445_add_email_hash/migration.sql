6bac5855e89f22dd81387021ec276822814fc985
Idhibhat Pankam <marginpankam@gmail.com>
2023-01-23 16:28:41 +0700

    feat: new user fields in schema, script for db reset

---
+++ prisma/migrations/20230123090445_add_email_hash/migration.sql
@@ -0,0 +1,17 @@
+/*
+  Warnings:
+
+  - A unique constraint covering the columns `[email]` on the table `user` will be added. If there are existing duplicate values, this will fail.
+  - Added the required column `email` to the `user` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `hash` to the `user` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- DropIndex
+DROP INDEX "user_username_key";
+
+-- AlterTable
+ALTER TABLE "user" ADD COLUMN     "email" TEXT NOT NULL,
+ADD COLUMN     "hash" TEXT NOT NULL;
+
+-- CreateIndex
+CREATE UNIQUE INDEX "user_email_key" ON "user"("email");
